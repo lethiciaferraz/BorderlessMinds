eac8d10b854af8c198a0d8c5f0ae0c41eec8dc47
lethiciaferraz <lethicia.ferraz@sptech.school>
2022-11-22 19:17:40 -0300

    Preparando para colocar o webdata viz

--- Banco de dados/projetoindiviualSQL.sql
+++ Banco de dados/projetoindiviualSQL.sql
@@ -1,13 +1,31 @@
-create database projetoIndividual;
+create database BorderlessMinds;
 
-use projetoIndividual
-
-
-
+use BorderlessMinds;
 
 create table usuario (
-idusuario in primary key auto_increment,
-nome varchar(45),
+idusuario int primary key auto_increment,
+nome varchar (250),
+pais varchar (45),
 email varchar (45),
-constraint chktipo check (email like "@"),
+constraint chktipo check (email like "@"), 
+senha varchar (45));
+
+create table mural (
+idmural int auto_increment,
+titulo varchar (45),
+descricao varchar(250),
+comentario varchar (250),
+fkusuario int,
+foreign key (fkusuario) references usuario (idusuario),
+primary key (idmural, fkusuario));
+
+create table idioma (
+idIdioma int primary key auto_increment,
+idioma varchar(45));
+
+create table votacao (
+fkidioma int,
+foreign key (fkidioma) references idioma(idIdioma),
+fkusuario int,
+foreign key (fkusuario) references usuario(idusuario));
 
